d1e97f1ca2acfbab5ee60e26a0147fe2a4c79ed3
Lucas Noffs Motta <lnoffs@hotmail.com>
2024-06-26 10:42:14 -0700

    Update

--- Sprite_Creator/messing_around.py
+++ Sprite_Creator/messing_around.py
@@ -1,5 +1,6 @@
 from engine import *
 from random import randint
+import sys
 
 
 #Test game
@@ -10,10 +11,10 @@ clock = py.time.Clock()
 
 
 #The objects from the engine classes
-
-
-
-
+random_color = (randint(0,255),randint(0,255),randint(0,255))
+list1 = FramesGenerator(5,(random_color),(randint(10,20),randint(10,50)))
+anima1 = FramesAnimation(list1.get_frames(),(400,200),0.5,screen)
+  
 
 
 
@@ -24,10 +25,9 @@ while True:
         if event.type == py.QUIT:
             py.quit()
             sys.exit()
+        
     
-    random_color = (randint(0,255),randint(0,255),randint(0,255))
-    list1 = FramesGenerator(10,(random_color),(randint(10,50),randint(10,50)))
-    anima1 = FramesAnimation(list1.get_frames(),(400,200),0.1,screen)
+    
     anima1.animation()
     anima1.screen_blit()
 
